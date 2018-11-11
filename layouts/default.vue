eb9e07f2bf2722de4152d12d2f5eaeb5675749db
Anand Chowdhary <anandchowdhary@gmail.com>
2018-11-11 09:36:44 +0100

    Add register

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,68 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation">
+    <nav class="navbar" role="navigation" aria-label="main navigation" style="margin-bottom: 2rem">
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/">
-          Ara
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
         </a>
 
-        <div class="navbar-burger">
-          <span/>
-          <span/>
-          <span/>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Home
+          </a>
+
+          <a class="navbar-item">
+            Documentation
+          </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <nuxt-link to="/auth/register" class="button is-primary">
+                <strong>Sign up</strong>
+              </nuxt-link>
+              <nuxt-link to="/auth/login" class="button is-light">
+                Log in
+              </nuxt-link>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
-
-    <section class="main-content columns">
-
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key">
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active">
-              <b-icon :icon="item.icon"/> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-
-    </section>
+    <div>
+      <nuxt />
+    </div>
   </div>
 </template>
 
@@ -49,8 +71,7 @@ export default {
   data() {
     return {
       items: [
-        { title: 'Home', icon: 'home', to: { name: 'index' } },
-        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
+        { title: 'Home', icon: 'home', to: { name: 'index' } }
       ]
     }
   }
