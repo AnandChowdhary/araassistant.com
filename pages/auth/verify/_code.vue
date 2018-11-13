5997af3308e5cbb3e7c080324b2c7a6da62f1591
Anand Chowdhary <anandchowdhary@gmail.com>
2018-11-13 23:19:49 +0100

    Add index

---
+++ pages/auth/verify/_code.vue
@@ -0,0 +1,36 @@
+<template>
+	<main>
+		<b-loading v-if="!done" :is-full-page="true" :active.sync="loading"></b-loading>
+		<div v-else class="content has-text-grey has-text-centered">
+			<p>
+				<b-icon class="ml" pack="fab" icon="google" size="is-large" />
+			</p>
+			<h1 class="title">Calendar Connected</h1>
+			<p>Your Google Calendar has been successfully connected.</p>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			loading: false,
+			done: true
+		}
+	},
+	mounted() {
+		if (this.$route.params.code) {
+			this.loading = true;
+			this.$axios.post("/verify", {
+				token: this.$route.params.code
+			}).then(() => {
+				this.$snackbar.open("Your email has been verified 👍");
+				this.$router.push("/auth/login");
+			}).catch(error => {
+				if (error.response.data.error) this.$snackbar.open({ type: "is-danger", message: error.response.data.error });
+			}).then(() => this.loading = false);
+		}
+	}
+}
+</script>
