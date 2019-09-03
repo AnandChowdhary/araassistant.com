a6198324f93e5f1464a114a346dafdd6bdfcd389
Anand Chowdhary <mail@anandchowdhary.com>
2019-09-03 12:31:18 +0530

    :sparkles: Save contact

--- pages/dashboard/_team/campaigns.vue
+++ pages/dashboard/_team/campaigns.vue
@@ -1,7 +1,30 @@
 <template>
   <main>
-    <h1>Campaigns</h1>
+    <h1>Contacts</h1>
     <p>List of outbound marketing campaigns will come here.</p>
+    <form @submit.prevent="search">
+      <input
+        v-model="query"
+        type="search"
+        placeholder="Enter a domain, e.g., oswaldlabs.com"
+      />
+      <button>Submit</button>
+    </form>
+    <div v-if="emails.length">
+      <h2>Leads</h2>
+      <ul>
+        <li v-for="(email, i) in emails" :key="`e${i}${email.email}`">
+          <strong>{{ email.first_name }} {{ email.last_name }}</strong>
+          {{ email.email }}
+          <button
+            class="button button--size-small"
+            @click.prevent="save(email)"
+          >
+            Save
+          </button>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
@@ -17,11 +40,23 @@ import Loading from "@/components/Loading.vue";
   }
 })
 export default class Dashboard extends Vue {
-  private async mounted() {
+  emails: any = [];
+  query = "";
+
+  private async search() {
+    if (!this.query) return;
     const result = (await this.$axios.get(
-      "/organizations/anands-assistant/leads/domain?q=oswaldlabs.com"
+      `/organizations/anands-assistant/leads/domain?q=${this.query}`
     )).data;
-    console.log("GOT RESULT", result);
+    this.query = "";
+    this.emails = result.data;
+  }
+
+  private async save(contact: any) {
+    await this.$axios.put(
+      `/organizations/anands-assistant/leads/contacts`,
+      contact
+    );
   }
 }
 </script>
